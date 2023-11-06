281aa05829ae231010595fe7aaca263931f5717e
Maksym Shokin <problaze7@gmail.com>
2023-11-06 19:52:38 +0200

    Load image logic

--- renderer/js/script.js
+++ renderer/js/script.js
@@ -2,23 +2,32 @@
 
 const form = document.querySelector('#img-form');
 
+const widthInput = document.querySelector('#width');
+const heightInput = document.querySelector('#height');
+
 function loadImage(e) {
   const file = e.target.files[0];
 
   if (!isFileImage(file)) {
-      alert('Please select an image file');
-        return;
+    alert('Please select an image file');
+    return;
   }
 
+  // Get original dimensions
+  const image = new Image();
+  image.src = URL.createObjectURL(file);
+  image.onload = function () {
+    widthInput.value = this.width;
+    heightInput.value = this.height;
+  };
+
   form.style.display = 'block';
-  document.querySelector(
-    '#filename'
-  ).innerHTML = file.name;
+  document.querySelector('#filename').innerHTML = file.name;
 }
 
 function isFileImage(file) {
-    const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
-    return file && acceptedImageTypes.includes(file['type'])
+  const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
+  return file && acceptedImageTypes.includes(file['type']);
 }
 
 document.querySelector('#img').addEventListener('change', loadImage);
