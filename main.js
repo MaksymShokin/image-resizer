dcff68d71d782c4a5d6464a0e438c46fa87e0eba
Maksym Shokin <problaze7@gmail.com>
2023-11-06 12:22:36 +0200

    Open dev tools

--- main.js
+++ main.js
@@ -3,12 +3,20 @@ const { app, BrowserWindow } = require('electron');
 
 const isMac = process.platform === 'darwin';
 
+// does not seem to work
+const isDev = process.env.NODE_ENV !== 'production';
+
 const createMainWindow = () => {
   const mainWindow = new BrowserWindow({
     title: 'Image resizer',
-    width: 500,
+    width: isDev ? 1000 : 500,
     height: 600
   });
+
+  // open dev tools if is in dev
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
   mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 };
